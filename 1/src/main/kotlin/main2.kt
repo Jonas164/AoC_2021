8d524bf2f2dcfdea0b716bf4248c32484c2e4117
Jonas Schreier <jonas.schreier@siemens.com>
2021-12-01 15:25:30 +0100

    Improved Part 2 Solution

--- 1/src/main/kotlin/main2.kt
+++ 1/src/main/kotlin/main2.kt
@@ -4,28 +4,14 @@ import CommonUtils.Factory.readFile
 
 fun main(args: Array<String>)
 {
-    val input = readFile("1/src/main/resources/input2.txt").map { e -> e.toInt() }
-    var prev = SlidingWindow(0, 0, 0)
-    var increased = 0;
-    for (i in 0..input.size - 3)
-    {
-        val win = SlidingWindow(input[i], input[i + 1], input[i + 2])
-        if (win.sum() > prev.sum())
-        {
-            increased++
-        }
-        prev = win;
-    }
 
-    println("Result: ${increased - 1}")
-}
-
-
-data class SlidingWindow(val first: Int, val second: Int, val third: Int)
-{
-    fun sum(): Int
-    {
-        return first + second + third
-    }
+    val result = readFile("1/src/main/resources/input.txt")
+        .asSequence()
+        .map { e -> e.toInt() }
+        .windowed(3)
+        .map { it.sum() }
+        .windowed(2)
+        .count { (e1, e2) -> e1 < e2 }
+    println("Result: $result")
 }
 

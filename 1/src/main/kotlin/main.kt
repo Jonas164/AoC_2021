bf472ab238b2aaf1518c6bd08c52e60929c00db9
Jonas Schreier <jonas.schreier@siemens.com>
2021-12-01 15:14:42 +0100

    Improved Part 1 Solution

--- 1/src/main/kotlin/main.kt
+++ 1/src/main/kotlin/main.kt
@@ -2,19 +2,10 @@ package main.kotlin
 
 fun main(args: Array<String>)
 {
-    val input = CommonUtils.readFile("1/src/main/resources/input.txt").map { e -> e.toInt() }
-    var increased = 0;
-    var prev = 0;
-    input.forEachIndexed { index, s ->
-        if (index != 0)
-        {
-            if (s.toInt() > prev)
-            {
-                increased++
-            }
-        }
-        prev = s.toInt()
-    }
-    println("Result: $increased")
+    val result = CommonUtils.readFile("1/src/main/resources/input.txt")
+        .map { e -> e.toInt() }
+        .windowed(2)
+        .count { (e1, e2) -> e1 < e2 }
+    println("Result: $result")
 }
 
